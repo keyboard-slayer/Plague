1595ffe29945fac66d31a528855388dfe8c9298b
0v3rl0w <dalcjor@libmail.eu>
2019-04-21 13:04:41 +0200

    Clean up the code and added targeted user functionality

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,23 @@
 use std::process::Command;
 
-fn get_username() -> String {
-    let output_utf8 = Command::new("whoami")
+fn run_command(command : &str, argument : &str) -> String {
+    let output_utf8 = Command::new(command)
+                      .arg(argument)
                       .output()
                       .expect("Failed to execute");
-
-    let output = String::from_utf8_lossy(&output_utf8.stdout).into_owned();
-    output.replace("\n", "")
+    String::from_utf8_lossy(&output_utf8.stdout).into_owned()
 }
 
-fn get_targets(link: &str) -> String {
-    let output_utf8 = Command::new("curl")
-                      .arg(link)
-                      .output()
-                      .expect("Failed to execute");
-    let output = String::from_utf8_lossy(&output_utf8.stdout).into_owned();
-    output
-}
+
 fn main() {
-    let username = get_username();
-    let targets = get_targets("http://localhost/user.txt");
-    println!("Username: {}", username);
-    println!("Targets: \n{}", targets);
+    let username      : String    = run_command("id", "-un").replace("\n", "");
+    let targets       : String    = run_command("curl", "http://localhost/user.txt");
+    let targets_vec   : Vec<&str> = targets.lines().collect();
+    let mut in_target : bool      = false;
+
+    for target in targets_vec {
+        if username == target {
+            in_target = true;
+        }
+    }
 }
